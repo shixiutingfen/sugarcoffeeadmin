fa15d4ac7df1317b4c1bf87e8052c73f8d20c559
shixiutingfen <345167830@qq.com>
2015-07-29 09:55:46 +0800

    utils

--- utils.py
+++ utils.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
+
+import hashlib
+"""
+	获取链接传参 如a=111&b=222
+	最后转化为{'a': '111', 'b': '222'}
+"""
 def getparam(str):
 	params=str.split("&")
 	l1=[]
@@ -8,6 +17,15 @@ def getparam(str):
 	resultmap=dict(zip(l1,l2))
 	return resultmap
 
+"""
+	md5密码加密
+"""
+def getenctry(password):
+	m = hashlib.md5()
+	m.update(password) 
+	return m.hexdigest()
+
 if __name__ == '__main__':
-	params=getparam("a=111&b=222")
-	print params
+	#password=getenctry("123")
+	param=getparam("a=111&b=222")
+	print param
